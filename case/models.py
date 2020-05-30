82d71b516f807021e8d2b922b0eab56ce5a45d77
safwan <safwanvalakindil@gmail.com>
2020-05-30 03:43:25 -0700

    Modified model case

--- case/models.py
+++ case/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Case(models.Model):
-    patient = models.ForeignKey(User, on_delete=models.CASCADE,related_name='case_patient')
-    receptionist = models.ForeignKey(User, on_delete=models.CASCADE,related_name='case_receptionist')
+    patient = models.CharField(max_length=250)
+    receptionist = models.CharField(max_length=250)
     description = models.CharField(max_length=500, default=None)
     filed_date = models.DateField()
     closed_date = models.DateField(default=None, null=True)
--- case/views.py
+++ case/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from case.forms import CaseForm
+from case.models import Case
 
 
 def add_case(request):
     form = CaseForm
+    cases = Case.objects.all()
+    if request.method == 'Post':
+        form = CaseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_case')
+
     context = {'form': form}
     return render(request, 'case/addcase.html', context)
